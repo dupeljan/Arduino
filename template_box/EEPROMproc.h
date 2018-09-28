04cb5cac9116bdbece65d75e9e9e7b11ce3a0e92
Dupeljan Linux <dupeljan@gmail.com>
2018-09-28 09:00:33 +0300

    Add EEPROM writing class NOT TESTED

---
+++ template_box/EEPROMproc.h
@@ -0,0 +1,25 @@
+#ifndef EEPROMproc_H
+#define EEPROMproc_H
+
+#include <EEPROM.h>
+
+#define STORAGE_SIZE	5 
+#define ZERO_ADDR	0
+
+
+class EEPROMproc{
+	byte curr_addr; 			// Current adress for writing
+public:
+	EEPROMproc(){
+		curr_addr = 0;
+		EEPROM.write(ZERO_ADDR, 0);
+	}
+	
+	void write(byte *v){			// Write key in memory
+		EEPROM.put(curr_addr * KEY_LENGTH , v);
+		curr_addr = ( curr_addr == STORAGE_SIZE - 1 )? 0 : curr_addr++;
+		EEPROM.write(ZERO_ADDR, curr_addr);
+	}
+	
+}
+#endif
